24e6677345196e4c0167736ed8c9c8c162793bfd
Florian Bruggisser <florian@bildspur.ch>
2020-11-27 23:04:14 +0100

    removed smicolon

--- src/main/kotlin/ch.bildspur/util/Mapping.kt
+++ src/main/kotlin/ch.bildspur/util/Mapping.kt
@@ -7,7 +7,7 @@ import kotlin.math.pow
 
 enum class Mapping(val mapping: (Double) -> Double, val inverseMapping: (Double) -> Double) {
     Linear({ it }, { it }),
-    SCurve({ 0.5 - cos(it * PI) * 0.5; }, { acos(-2 * (it - 0.5)) / PI }),
+    SCurve({ 0.5 - cos(it * PI) * 0.5 }, { acos(-2 * (it - 0.5)) / PI }),
     Quad({ it.pow(2.0) }, { it.pow(1.0 / 2.0) }),
     Cube({ it.pow(3.0) }, { it.pow(1.0 / 3.0) }),
     Quart({ it.pow(4.0) }, { it.pow(1.0 / 4.0) }),
