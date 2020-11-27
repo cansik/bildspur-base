1619edf1bb432eae13db73bc99ee87435abbb046
Florian Bruggisser <florian@bildspur.ch>
2020-11-27 11:13:04 +0100

    added basic easing methods

--- src/main/kotlin/ch.bildspur/util/Easing.kt
+++ src/main/kotlin/ch.bildspur/util/Easing.kt
@@ -1,33 +1,55 @@
-@file:Suppress("NAME_SHADOWING")
-
 package ch.bildspur.util
 
+import ch.bildspur.math.pow
+import kotlin.math.cos
+import kotlin.math.PI
+import kotlin.math.sin
+import kotlin.math.sqrt
+
 /**
- * Created by cansik on 08.06.17.
+ * All easings are visualized here:
+ * https://easings.net/
  */
-object Easing {
-    // t = current time, b = start value, c = change in value, d = duration
-    fun easeInQuad(t: Float, b: Float, c: Float, d: Float): Float {
-        var v = t
-        v /= d
-        return c * v * v + b
-    }
+enum class Easing(val method: (Float) -> Float) {
+    Linear({ it }),
 
-    fun linearTween(t: Float, b: Float, c: Float, d: Float): Float {
-        return c * t / d + b
-    }
+    EaseInSine({ 1 - cos((it * PI) / 2f).toFloat() }),
+    EaseOutSine({ sin((it * PI) / 2f).toFloat() }),
+    EaseInOutSine({ (-(cos(PI * it) - 1) / 2f).toFloat() }),
 
-    fun easeOutQuad(t: Float, b: Float, c: Float, d: Float): Float {
-        var v = t
-        v /= d
-        return -c * v * (v - 2) + b
-    }
+    EaseInQuad({ it * it }),
+    EaseOutQuad({ it * (2 - it) }),
+    EaseInOutQuad({ if (it < .5) 2 * it * it else -1 + (4 - 2 * it) * it }),
 
-    fun easeInSine(t: Float, b: Float, c: Float, d: Float): Float {
-        return Math.round(-c * Math.cos(t / d * (Math.PI / 2.0)) + c.toDouble() + b.toDouble()).toFloat()
-    }
+    EaseInCubic({ it * it * it }),
+    EaseOutCubic({ var t = it; (--t) * t * t + 1 }),
+    EaseInOutCubic({ if (it < .5) 4 * it * it * it else (it - 1) * (2 * it - 2) * (2 * it - 2) + 1 }),
 
-    fun easeOutSine(t: Float, b: Float, c: Float, d: Float): Float {
-        return Math.round(c * Math.sin(t / d * (Math.PI / 2.0)) + b).toFloat()
+    EaseInQuart({ it * it * it * it }),
+    EaseOutQuart({ var t = it; 1 - (--t) * t * t * t }),
+    EaseInOutQuart({ var t = it; if (t < .5) 8 * t * t * t * t else 1 - 8 * (--t) * t * t * t }),
+
+    EaseInQuint({ it * it * it * it * it }),
+    EaseOutQuint({ var t = it; 1 + (--t) * t * t * t * t }),
+    EaseInOutQuint({ var t = it; if (t < .5) 16 * t * t * t * t * t else 1 + 16 * (--t) * t * t * t * t }),
+
+    EaseInExponential({ if (it == 0f) 0f else pow(2f, 10 * it - 10) }),
+    EaseOutExponential({ if (it == 1f) 1f else 1 - pow(2f, -10 * it) }),
+    EaseInOutExponential(
+            {
+                when (it) {
+                    0f -> 0f
+                    1f -> 1f
+                    else -> if (it < 0.5f) pow(2f, 20 * it - 10) / 2f else (2 - pow(2f, -20 * it + 10)) / 2f
+                }
+            }),
+
+    EaseInCircular({ 1 - sqrt(1 - pow(it, 2f)) }),
+    EaseOutCircular({ sqrt(1 - pow(it - 1, 2f)) }),
+    EaseInOutCircular({ if (it < 0.5) (1 - sqrt(1 - pow(2 * it, 2f))) / 2 else (sqrt(1 - pow(-2 * it + 2, 2f)) + 1) / 2 }),
+    ;
+
+    operator fun invoke(t: Float): Float {
+        return method(t)
     }
 }
