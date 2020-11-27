ef6673a586e9b9cdab644092ba8b41c15a40741b
Florian Bruggisser <florian@bildspur.ch>
2020-11-27 13:47:05 +0100

    fixed mapping method

--- src/main/kotlin/ch.bildspur/util/Mapping.kt
+++ src/main/kotlin/ch.bildspur/util/Mapping.kt
@@ -1,5 +1,16 @@
 package ch.bildspur.util
 
+import kotlin.math.PI
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.pow
+
 enum class Mapping(val mapping: (Double) -> Double, val inverseMapping: (Double) -> Double) {
-    Linear({it}, {it})
+    Linear({ it }, { it }),
+    SCurve({ 0.5 - cos(it * PI) * 0.5; }, { acos(-2 * (it - 0.5)) / PI }),
+    Quad({ it.pow(2.0) }, { it.pow(1.0 / 2.0) }),
+    Cube({ it.pow(3.0) }, { it.pow(1.0 / 3.0) }),
+    Quart({ it.pow(4.0) }, { it.pow(1.0 / 4.0) }),
+    Quint({ it.pow(5.0) }, { it.pow(1.0 / 5.0) }),
+    ;
 }
--- src/test/kotlin/ch/bildspur/test/EasingAndMappingTest.kt
+++ src/test/kotlin/ch/bildspur/test/EasingAndMappingTest.kt
@@ -2,6 +2,7 @@ package ch.bildspur.test
 
 import ch.bildspur.util.Easing
 import ch.bildspur.util.Mapping
+import ch.bildspur.util.isInMargin
 import org.junit.Test
 
 class EasingAndMappingTest {
@@ -17,12 +18,14 @@ class EasingAndMappingTest {
 
     @Test
     fun mappingTest() {
-        val mapping = Mapping.Linear
-        val value = 0.2
+        val mapping = Mapping.SCurve
+        for(i in 0..100) {
+            val value = 0.01 * i
+            val mapped = mapping.mapping(value)
+            val inverted = mapping.inverseMapping(mapped)
 
-        val mapped = mapping.mapping(value)
-        val inverted = mapping.inverseMapping(mapped)
-
-        assert(mapped == inverted)
+            println("In: $value Mapped: $mapped Inverted: $inverted")
+            assert(value.isInMargin(inverted, 1.0E-8))
+        }
     }
 }
